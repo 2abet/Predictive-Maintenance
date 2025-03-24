df99a03f20796d808509c8f634ad2f7341b766e3
Akinyemi Arabambi <32962207+2abet@users.noreply.github.com>
2025-03-24 10:48:21 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,10 +9,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import (
+    confusion_matrix,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report
+)
 
 st.set_page_config(page_title="Predictive Maintenance Dashboard", layout="centered")
 
